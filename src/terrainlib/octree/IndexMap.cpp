9a1fddbd51eb2a7415d86b3d633600d4ca1d181b
Martin Braunsperger <e11909911@student.tuwien.ac.at>
2025-05-29 23:21:34 +0200

    Split IndexMap and Space into .h and .cpp

---
+++ src/terrainlib/octree/IndexMap.cpp
@@ -0,0 +1,172 @@
+#include "octree/IndexMap.h"
+#include "log.h"
+
+namespace octree {
+
+std::optional<NodeStatus> IndexMap::get(Id id) const {
+    const NodeStatus* status = this->get_raw(id);
+    if (status != nullptr) {
+        return *status;
+    } else {
+        return std::nullopt;
+    }
+}
+
+void IndexMap::add(Id id) {
+    NodeStatus* status = this->get_raw(id);
+    if (status != nullptr) {
+        switch (*status) {
+            case NodeStatus::Inner:
+            case NodeStatus::Leaf:
+                // Nothing to do
+                break;
+            case NodeStatus::Virtual:
+                this->set_raw(id, NodeStatus::Inner);
+                break;
+        }
+        return;
+    }
+
+    const auto parent_opt = id.parent();
+    if (!parent_opt.has_value()) {
+        assert(id.is_root());
+        if (this->empty()) {
+            this->set_raw(id, NodeStatus::Leaf);
+            return;
+        } else {
+            UNREACHABLE();
+            return;
+        }
+    }
+    
+    const auto parent = parent_opt.value();
+    const NodeStatus* parent_status = this->get_raw(parent);
+    if (parent_status == nullptr) {
+        this->add(parent);
+        this->set_raw(parent, NodeStatus::Virtual);
+        this->set_raw(id, NodeStatus::Leaf);
+        return;
+    }
+
+    switch (*parent_status) {
+        case NodeStatus::Leaf:
+            this->set_raw(parent, NodeStatus::Inner);
+            this->set_raw(id, NodeStatus::Leaf);
+            break;
+        case NodeStatus::Inner:
+        case NodeStatus::Virtual:
+            this->set_raw(id, NodeStatus::Leaf);
+            break;
+    }
+}
+
+bool IndexMap::remove(Id id) {
+    NodeStatus* status = this->get_raw(id);
+    if (status == nullptr) {
+        return false;
+    }
+
+    switch (*status) {
+        case NodeStatus::Inner:
+            *status = NodeStatus::Virtual;
+            return true;
+        case NodeStatus::Virtual:
+            // Nothing to do
+            return false;
+        case NodeStatus::Leaf:
+            this->remove_raw(id);
+            this->update_parent_after_remove(id);
+            return true;
+    }
+
+    UNREACHABLE();
+}
+
+bool IndexMap::is_present(Id id) const {
+    auto it = this->_index.find(id);
+    return it != this->_index.end();
+}
+bool IndexMap::is_absent(Id id) const {
+    return !this->is_present(id);
+}
+bool IndexMap::is(NodeStatus status, Id id) const {
+    return this->get(id) == status;
+}
+
+void IndexMap::clear() {
+    this->_index.clear();
+}
+bool IndexMap::empty() const {
+    return this->_index.empty();
+}
+size_t IndexMap::size() const {
+    return this->_index.size();
+}
+
+IndexMap::const_iterator IndexMap::begin() const {
+    return this->_index.begin();
+}
+IndexMap::const_iterator IndexMap::end() const {
+    return this->_index.end();
+}
+IndexMap::const_iterator IndexMap::cbegin() const {
+    return this->_index.cbegin();
+}
+IndexMap::const_iterator IndexMap::cend() const {
+    return this->_index.cend();
+}
+
+
+NodeStatus* IndexMap::get_raw(Id id) {
+    if (auto it = this->_index.find(id); it != this->_index.end()) {
+        return &it->second;
+    }
+    return nullptr;
+}
+const NodeStatus* IndexMap::get_raw(Id id) const {
+    if (auto it = this->_index.find(id); it != this->_index.end()) {
+        return &it->second;
+    }
+    return nullptr;
+}
+void IndexMap::set_raw(Id id, NodeStatus status) {
+    this->_index[id] = status;
+}
+void IndexMap::remove_raw(Id id) {
+    this->_index.erase(id);
+}
+
+void IndexMap::update_parent_after_remove(Id id) {
+    const auto parent_opt = id.parent();
+    if (!parent_opt.has_value()) {
+        return;
+    }
+    
+    const auto parent = parent_opt.value();
+    NodeStatus* parent_status = this->get_raw(parent);
+    assert(parent_status != nullptr);
+
+    // We know there are children since we just removed one
+    assert(parent.has_children());
+    const auto siblings = parent.children().value();
+    for (const auto& sibling : siblings) {
+        if (this->is_present(sibling) && sibling != id) {
+            return;
+        }
+    }
+
+    switch (*parent_status) {
+        case NodeStatus::Inner:
+            *parent_status = NodeStatus::Leaf;
+            break;
+        case NodeStatus::Virtual:
+            this->remove_raw(parent);
+            this->update_parent_after_remove(parent);
+            break;
+        case NodeStatus::Leaf:
+            UNREACHABLE();
+            break;
+    }
+}
+
+}
--- src/terrainlib/octree/IndexMap.h
+++ src/terrainlib/octree/IndexMap.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <unordered_map>
+#include <optional>
 
 #include <zpp_bits.h>
 
 #include "octree/Id.h"
 #include "octree/NodeStatus.h"
-#include "log.h"
 
 namespace octree {
 
@@ -16,179 +16,34 @@ public:
     using iterator = Container::iterator;
     using const_iterator = Container::const_iterator;
 
-    std::optional<NodeStatus> get(Id id) const {
-        const NodeStatus* status = this->get_raw(id);
-        if (status != nullptr) {
-            return *status;
-        } else {
-            return std::nullopt;
-        }
-    }
+    std::optional<NodeStatus> get(Id id) const;
+    void add(Id id);
+    bool remove(Id id);
 
-    void add(Id id) {
-        NodeStatus* status = this->get_raw(id);
-        if (status != nullptr) {
-            switch (*status) {
-                case NodeStatus::Inner:
-                case NodeStatus::Leaf:
-                    // Nothing to do
-                    break;
-                case NodeStatus::Virtual:
-                    this->set_raw(id, NodeStatus::Inner);
-                    break;
-            }
-            return;
-        }
+    bool is_present(Id id) const;
+    bool is_absent(Id id) const;
+    bool is(NodeStatus status, Id id) const;
 
-        const auto parent_opt = id.parent();
-        if (!parent_opt.has_value()) {
-            assert(id.is_root());
-            if (this->empty()) {
-                this->set_raw(id, NodeStatus::Leaf);
-                return;
-            } else {
-                UNREACHABLE();
-                return;
-            }
-        }
-        
-        const auto parent = parent_opt.value();
-        const NodeStatus* parent_status = this->get_raw(parent);
-        if (parent_status == nullptr) {
-            this->add(parent);
-            this->set_raw(parent, NodeStatus::Virtual);
-            this->set_raw(id, NodeStatus::Leaf);
-            return;
-        }
+    void clear();
+    bool empty() const;
+    size_t size() const;
 
-        switch (*parent_status) {
-            case NodeStatus::Leaf:
-                this->set_raw(parent, NodeStatus::Inner);
-                this->set_raw(id, NodeStatus::Leaf);
-                break;
-            case NodeStatus::Inner:
-            case NodeStatus::Virtual:
-                this->set_raw(id, NodeStatus::Leaf);
-                break;
-        }
-    }
+    const_iterator begin() const;
+    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
 
-    bool remove(Id id) {
-        NodeStatus* status = this->get_raw(id);
-        if (status == nullptr) {
-            return false;
-        }
+    NodeStatus* get_raw(Id id);
+    const NodeStatus* get_raw(Id id) const;
+    void set_raw(Id id, NodeStatus status);
+    void remove_raw(Id id);
 
-        switch (*status) {
-            case NodeStatus::Inner:
-                *status = NodeStatus::Virtual;
-                return true;
-            case NodeStatus::Virtual:
-                // Nothing to do
-                return false;
-            case NodeStatus::Leaf:
-                this->remove_raw(id);
-                this->update_parent_after_remove(id);
-                return true;
-        }
-
-        UNREACHABLE();
-    }
-
-    bool is_present(Id id) const {
-        auto it = this->_index.find(id);
-        return it != this->_index.end();
-    }
-    bool is_absent(Id id) const {
-        return !this->is_present(id);
-    }
-    bool is(NodeStatus status, Id id) const {
-        return this->get(id) == status;
-    }
-
-    void clear() {
-        this->_index.clear();
-    }
-    bool empty() const {
-        return this->_index.empty();
-    }
-    size_t size() const {
-        return this->_index.size();
-    }
-
-    const_iterator begin() const {
-        return this->_index.begin();
-    }
-    const_iterator end() const {
-        return this->_index.end();
-    }
-    const_iterator cbegin() const {
-        return this->_index.cbegin();
-    }
-    const_iterator cend() const {
-        return this->_index.cend();
-    }
-
-    
-    NodeStatus* get_raw(Id id) {
-        if (auto it = this->_index.find(id); it != this->_index.end()) {
-            return &it->second;
-        }
-        return nullptr;
-    }
-    const NodeStatus* get_raw(Id id) const {
-        if (auto it = this->_index.find(id); it != this->_index.end()) {
-            return &it->second;
-        }
-        return nullptr;
-    }
-    void set_raw(Id id, NodeStatus status) {
-        this->_index[id] = status;
-    }
-    void remove_raw(Id id) {
-        this->_index.erase(id);
-    }
-
+    using serialize = zpp::bits::members<1>;
+    friend zpp::bits::access;
 
 private:
     Container _index;
-
-    void update_parent_after_remove(Id id) {
-        const auto parent_opt = id.parent();
-        if (!parent_opt.has_value()) {
-            return;
-        }
-        
-        const auto parent = parent_opt.value();
-        NodeStatus* parent_status = this->get_raw(parent);
-        assert(parent_status != nullptr);
-
-        // We know there are children since we just removed one
-        assert(parent.has_children());
-        const auto siblings = parent.children().value();
-        for (const auto& sibling : siblings) {
-            if (this->is_present(sibling) && sibling != id) {
-                return;
-            }
-        }
-
-        switch (*parent_status) {
-            case NodeStatus::Inner:
-                *parent_status = NodeStatus::Leaf;
-                break;
-            case NodeStatus::Virtual:
-                this->remove_raw(parent);
-                this->update_parent_after_remove(parent);
-                break;
-            case NodeStatus::Leaf:
-                UNREACHABLE();
-                break;
-        }
-    }
-
-
-public:
-    using serialize = zpp::bits::members<1>;
-    friend zpp::bits::access;
+    void update_parent_after_remove(Id id);
 };
-}
+
+} // namespace octree

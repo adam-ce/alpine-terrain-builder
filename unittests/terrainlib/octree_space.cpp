f82b02b3f4eafa673268f2c7c26ef093fd8c6994
Martin Braunsperger <e11909911@student.tuwien.ac.at>
2025-06-07 17:54:38 +0200

    Fix: warning in terrainlib unittest

--- unittests/terrainlib/octree_space.cpp
+++ unittests/terrainlib/octree_space.cpp
@@ -75,7 +75,9 @@ TEST_CASE("find_smallest_node_encompassing_bounds returns smallest valid node", 
     }
 
     // Now check that no child of this node fully contains the target
-    for (const auto &child_id : id.children().value()) {
+    REQUIRE(id.has_children());
+    const auto children = id.children().value();
+    for (const auto &child_id : children) {
         Bounds child_bounds = space.get_node_bounds(child_id);
         bool all_corners_inside = true;
         for (const auto &corner : radix::geometry::corners(target)) {

7ae090b74f0f28bd4a0091fd76cd2bbda8bf3ae8
Martin Braunsperger <e11909911@student.tuwien.ac.at>
2026-07-17 15:38:41 +0200

    Reduce cluster merge epsilon to 1/1'000'000 of node bounds

--- src/dag_builder/build.cpp
+++ src/dag_builder/build.cpp
@@ -256,7 +256,7 @@ dag::ClusterBatch combine_input_and_inner(
 
 // Compute epsilon value for merging clusters based on the size of the node bounds.
 double compute_epsilon(const radix::geometry::Aabb3d &bounds) {
-    return glm::compAdd(bounds.size()) / 10000;
+    return glm::compAdd(bounds.size()) / 1'000'000;
 }
 
 // Build a single target node by loading input clusters (preserved as-is) and

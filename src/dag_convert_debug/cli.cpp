a7e49a35df994bc6cb19b6399b502a66c498f074
Martin Braunsperger <e11909911@student.tuwien.ac.at>
2026-07-30 22:53:57 +0200

    feat: Improve cli interface for dag debug convert

--- src/dag_convert_debug/cli.cpp
+++ src/dag_convert_debug/cli.cpp
@@ -31,12 +31,11 @@ Args cli::parse(int argc, const char *const *argv) {
         .log_level = spdlog::level::level_enum::info,
     };
 
-    app.add_option("--input", args.input_path, "Path to a DAG storage folder, or a single node file")
+    app.add_option("input,--input", args.input_path, "Path to a DAG storage folder, or a single node file")
         ->required()
         ->check(CLI::ExistingPath);
 
-    app.add_option("--output", args.output_path, "Path to write debug meshes to: a folder if --input is a folder, or a mesh file if --input is a file")
-        ->required();
+    app.add_option("output,--output", args.output_path, "Path to write debug meshes to: a folder if --input is a folder, or a mesh file if --input is a file");
 
     app.add_option("--verbosity", args.log_level, "Verbosity level of logging")
         ->transform(CLI::CheckedTransformer(log_level_names, CLI::ignore_case))
@@ -46,6 +45,15 @@ Args cli::parse(int argc, const char *const *argv) {
         app.parse(argc, argv);
     } catch (const CLI::ParseError &e) {
         std::exit(app.exit(e));
+    }
+
+    if (args.output_path.empty()) {
+        auto stem = args.input_path.stem();
+        auto ext = args.input_path.extension();
+        if (!ext.empty()) {
+            ext = ".glb";
+        }
+        args.output_path = args.input_path.parent_path().append(fmt::format("{}-debug{}", stem, ext));
     }
 
     return args;
